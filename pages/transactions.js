ee8c8448928de3d5344eee427480143a4c3c6893
Hafez Usman <55655270+hafezus@users.noreply.github.com>
2021-05-26 01:46:54 +0400

    Update transactions.js

--- pages/transactions.js
+++ pages/transactions.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "../Components/navbar";
-import Footer from "../Components/footer";
-import WalletTransactions from "../Components/transactions/walletTransactions";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import WalletTransactions from "../Components/transactions/WalletTransactions";
 import Head from "next/head";
 import { initializeContracts } from "../web3.js";
-import Layout, { siteTitle } from "../Components/layout";
+import Layout, { siteTitle } from "../Components/Layout";
 import { useState, useEffect } from "react";
 const Transactions = () => {
 	const [data, setData] = useState([]);
@@ -16,12 +16,10 @@ const Transactions = () => {
 
 	return (
 		<div className="bg-gray-100">
-			{/* className={utilStyles} */}
 			<Head>
 				<title>{siteTitle}</title>
 			</Head>
 			<Navbar />
-			{/* <Wallet /> */}
 			<WalletTransactions data={data} />
 			<Footer />
 		</div>
