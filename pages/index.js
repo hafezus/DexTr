7532fc25e543831911c2b58b4710ec6f822a15fa
Hafez Usman <55655270+hafezus@users.noreply.github.com>
2021-05-26 01:45:51 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,12 @@
 import Head from "next/head";
-import Layout, { siteTitle } from "../Components/layout";
-//import utilStyles from "../styles/utils.module.css";
-import Navbar from "../Components/navbar";
-import Trade from "../Components/trade/trade";
-import Footer from "../Components/footer";
-import axios from "axios";
-
-/* 
-	index 	--> trade			--> buy & sell dxt + fetch subgraph API data
-			--> transactions	--> Fetch Buy & Sell events
-*/
+import Layout, { siteTitle } from "../Components/Layout";
+import Navbar from "../Components/Navbar";
+import Trade from "../Components/trade/Trade";
+import Footer from "../Components/Footer";
 
 export default function Home() {
 	return (
 		<div className="bg-gray-100">
-			{/* overflow hidden prevents Wallet contents from leaking into Footer */}
-			{/* className={utilStyles} */}
 			<Head>
 				<title>{siteTitle}</title>
 			</Head>
